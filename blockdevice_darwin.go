3b388b4e888b0781449d1da1e52c10e936b615e9
Baptiste Canton <bat@sbz.fr>
2019-08-16 21:52:26 +0200

    darwin support

---
+++ blockdevice_darwin.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+)
+
+// #include <sys/disk.h>
+// #include <sys/ioctl.h>
+//
+// unsigned long long get_size(int fd) {
+//	unsigned long long blocksize = 0;
+//	unsigned long long blockcount = 0;
+//
+// 	ioctl(fd, DKIOCGETBLOCKSIZE, &blocksize);
+// 	ioctl(fd, DKIOCGETBLOCKCOUNT, &blockcount);
+//  return blockcount*blocksize;
+// }
+import "C"
+
+func getBlockDeviceSize(f *os.File) int64 {
+	size := int64(C.get_size(C.int(f.Fd())))
+	return size
+}
