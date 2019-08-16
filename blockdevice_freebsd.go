00855ab715b29f4fa710e5ebf133a17bdb801efd
Your Name <you@example.com>
2019-08-16 23:14:29 +0200

    add freebsd support

---
+++ blockdevice_freebsd.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+)
+
+// #include <sys/disk.h>
+// #include <sys/ioctl.h>
+//
+// unsigned long long get_size(int fd) {
+//	unsigned long long file_size_in_bytes = 0;
+// 	ioctl(fd, DIOCGMEDIASIZE, &file_size_in_bytes);
+//	return file_size_in_bytes;
+// }
+import "C"
+
+func getBlockDeviceSize(f *os.File) int64 {
+	size := int64(C.get_size(C.int(f.Fd())))
+	return size
+}
