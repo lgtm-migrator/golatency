27f001e4a403509fcf91c46b4ccb95014c929222
Baptiste Canton <bat@sbz.fr>
2021-02-20 22:42:15 +0100

    add mt

--- golatency.go
+++ golatency.go
@@ -20,13 +20,15 @@ import (
 
 func main() {
 	var (
+		routines  int
 		count     int
 		nocache   bool
 		seqTest   bool
 		quickTest bool
 		seed      bool
 	)
-	flag.IntVar(&count, "count", 100, "how many read to do")
+	flag.IntVar(&routines, "p", 1, "how many goroutines")
+	flag.IntVar(&count, "count", 1000, "how many read to do (per goroutine)")
 	flag.BoolVar(&nocache, "nocache", false, "bypass OS Cache")
 	flag.BoolVar(&seqTest, "T", false, "finish by a sequential complete file read")
 	flag.BoolVar(&quickTest, "t", false, "finish by a quick sequential complete file read (10s max)")
@@ -83,48 +85,72 @@ func main() {
 		os.Exit(-1)
 	}
 
-	log.Printf("size: %v (%v), doing %v req...", emphasis(byteCountDecimal(size)), byteCountBinary(size), count)
+	log.Printf("size: %v (%v), doing %v req * %v ...", emphasis(byteCountDecimal(size)), byteCountBinary(size), count, routines)
 
-	var b []byte
-	if nocache {
-		b = directio.AlignedBlock(directio.BlockSize)
-		if int64(len(b)) > size {
-			log.Printf("-nocache needs a file at least %v B long, we will probably fail", len(b))
-		}
-	} else {
-		b = make([]byte, 1)
-	}
-	if seed {
-		dummySeed := int64(os.Getpid()) + time.Now().UnixNano()
-		//log.Printf("seeding is set to %v", emphasis(dummySeed))
-		log.Printf("seeding nonsense as requested.")
-		rand.Seed(dummySeed)
-	}
-	var myrand int64
-	start := time.Now()
-	for index := 0; index < count; index++ {
-		var alignSize int64 = directio.AlignSize
-		if nocache && alignSize != 0 {
-			// random aligned offset
-			myrand = rand.Int63n((size-1)/alignSize) * alignSize
-		} else {
-			//random offset
-			myrand = rand.Int63n(size - 1)
-		}
-		_, err = f.ReadAt(b, myrand)
-		if err != nil {
-			log.Fatal(err)
-		}
+	results := make(chan time.Duration)
+	realstart := time.Now()
+
+	for i := 0; i < routines; i++ {
+		go func(n int) {
+			var b []byte
+			if nocache {
+				b = directio.AlignedBlock(directio.BlockSize)
+				if int64(len(b)) > size {
+					if n == 0 {
+						log.Printf("(%v) -nocache needs a file at least %v B long, we will probably fail", n, len(b))
+					}
+				}
+			} else {
+				b = make([]byte, 1)
+			}
+			if seed {
+				dummySeed := int64(os.Getpid()) + time.Now().UnixNano()
+				//log.Printf("seeding is set to %v", emphasis(dummySeed))
+				if n == 0 {
+					log.Printf("(%v) seeding nonsense as requested.", n)
+				}
+				rand.Seed(dummySeed)
+			} else {
+				dummySeed := n
+				//log.Printf("seeding is set to %v", emphasis(dummySeed))
+				rand.Seed(int64(dummySeed))
+			}
+			var myrand int64
+			start := time.Now()
+			for index := 0; index < count; index++ {
+				var alignSize int64 = directio.AlignSize
+				if nocache && alignSize != 0 {
+					// random aligned offset
+					myrand = rand.Int63n((size-1)/alignSize) * alignSize
+				} else {
+					//random offset
+					myrand = rand.Int63n(size - 1)
+				}
+				_, err = f.ReadAt(b, myrand)
+				if err != nil {
+					log.Printf("(%v) %v", n, err)
+				}
+			}
+			results <- time.Since(start)
+		}(i)
 	}
 
-	t := time.Since(start)
-	log.Printf("total time: %v ns (%s) for %v requests", t.Nanoseconds(), t, count)
-	durationPerReq, _ := time.ParseDuration(strconv.Itoa(int(t.Nanoseconds()/int64(count))) + "ns")
-	log.Printf("per rq time: %v ns (%s)", t.Nanoseconds()/int64(count), emphasis(durationPerReq))
+	var totaltime time.Duration
+	for i := 0; i < routines; i++ {
+		totaltime += <-results
+	}
+	realtime := time.Since(realstart)
+
+	log.Printf("totaltime: %v ns (%s) for %v req/routine (%v goroutines)", totaltime.Nanoseconds(), totaltime, count, routines)
+	log.Printf("realtime:  %v ns (%s) for %v req/routine (%v goroutines)", realtime.Nanoseconds(), realtime, count, routines)
+	totalDurationPerReq, _ := time.ParseDuration(strconv.Itoa(int(totaltime.Nanoseconds()/int64(routines*count))) + "ns")
+	realDurationPerReq, _ := time.ParseDuration(strconv.Itoa(int(realtime.Nanoseconds()/int64(routines*count))) + "ns")
+	log.Printf("total per req time: %s", emphasis(totalDurationPerReq))
+	log.Printf("real  per req time: %s", emphasis(realDurationPerReq))
 	log.Printf("bytes requested (%v blocks): %v (512) | %v (4096)",
 		count,
-		byteCountDecimal(int64(512*count)),
-		byteCountDecimal(int64(4096*count)))
+		byteCountDecimal(int64(512*count*routines)),
+		byteCountDecimal(int64(4096*count*routines)))
 
 	if seqTest || quickTest {
 		_, err := f.Seek(0, 0)
